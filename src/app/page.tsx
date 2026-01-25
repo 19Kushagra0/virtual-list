3a3ac21c02ca11261f5096cba6a3f29b686c4789
19Kushagra0 <19Kushagra0@gmail.com>
2026-01-25 16:59:24 +0530

    Kushagra dev (#2)
    * FAKE SCROLL created
    
    * log scroll position in scroll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,43 @@
 "use client";
 
+import { useRef } from "react";
+
 const row_height = 40;
 const container_height = 400;
 const total_rows = 50000;
 
 export default function Home() {
+  // typescript saftey check
+  // scrollRef could be a element or null
+  // pointing to element
+  const scrollRef = useRef<HTMLDivElement | null>(null);
+
+  //  saving scroll number
+  const scrollTopRef = useRef(0);
+
+  // typescript saftey check
+  // scrollRef is not null here
+  const handleScroll = () => {
+    if (scrollRef.current === null) {
+      return;
+    }
+    // scrollTop telling as at what number we scrolled
+    scrollTopRef.current = scrollRef.current.scrollTop;
+    console.log(scrollTopRef.current);
+  };
+
   return (
     <div
+      // You don’t need ref to listen to onScroll,
+      // and you don’t need ref to get scrollTop if
+      //  you use the event — but you do need ref if
+      //  you want persistent access to the scrollable
+      // element
+
+      // scrollRef is in element now
+      ref={scrollRef}
+      // event listener tell us when we scoll
+      onScroll={handleScroll}
       className=" w-full overflow-auto bg-red-500"
       style={{ height: container_height }}
     >
